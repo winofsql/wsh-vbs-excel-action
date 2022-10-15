558904a1388388686e90912071469396c7b0674d
sworc <lightbox@nifty.com>
2022-10-15 23:49:42 +0900

    Add files via upload

--- php/load-data.php
+++ php/load-data.php
@@ -1,1 +1,24 @@
+<?php
 
+$ExcelApp = new com("Excel.Application") or die("Unable to instantiate Excel");
+
+// 表示する
+$ExcelApp->Visible = true;
+// 警告を出さないようにする
+$ExcelApp->DisplayAlerts = false;
+
+// 開く
+$workbook = $ExcelApp->Workbooks->Open( getcwd() ."\\syain.xlsx" );
+// 最大化
+$ExcelApp->ActiveWindow->WindowState = -4137;
+
+// 保存した事にする
+$workook->Saved = true;
+
+// アプリを終了
+$ExcelApp->Quit();
+
+// オブジェクト初期化
+$ExcelApp = null;
+
+?>
