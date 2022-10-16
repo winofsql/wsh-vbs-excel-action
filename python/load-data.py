6e329b987881c9f237af38b0cc1743000882556a
sworc <lightbox@nifty.com>
2022-10-16 20:44:05 +0900

    Add files via upload

--- python/load-data.py
+++ python/load-data.py
@@ -1,1 +1,22 @@
+import win32com.client
+import os
+from tkinter import messagebox
+
+curdir = os.getcwd()
+
+ExcelApp = win32com.client.Dispatch("Excel.Application")
+
+ExcelApp.Visible = True
+
+ExcelApp.DisplayAlerts = False
+
+workbook = ExcelApp.Workbooks.Open( curdir + "\\syain.xlsx" )
+
+ExcelApp.ActiveWindow.WindowState = -4137
+
+messagebox.showinfo("タイトル", "STOP")
+
+workbook.Saved = True
+
+ExcelApp.Quit()
 
