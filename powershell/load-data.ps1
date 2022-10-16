131069e32cf6900791db04d6fa3d8d225cda26f0
sworc <lightbox@nifty.com>
2022-10-16 13:21:00 +0900

    Add files via upload

--- powershell/load-data.ps1
+++ powershell/load-data.ps1
@@ -1,1 +1,27 @@
+# powershell .\load-data.ps1
+
+Add-Type -Assembly System.Windows.Forms
+
+$ExcelApp = New-Object -ComObject Excel.Application
+
+# 表示する
+$ExcelApp.Visible = $true
+# 警告を出さないようにする
+$ExcelApp.DisplayAlerts = $false
+
+# 開く
+$workbook = $ExcelApp.Workbooks.Open( (Convert-Path .) + "\syain.xlsx" )
+# 最大化
+$ExcelApp.ActiveWindow.WindowState = -4137
+
+[System.Windows.Forms.MessageBox]::Show("STOP", "タイトル")
+
+# 保存した事にする
+$workook.Saved = $true
+
+# アプリを終了
+$ExcelApp.Quit()
+
+# オブジェクト初期化
+$ExcelApp = $null
 
